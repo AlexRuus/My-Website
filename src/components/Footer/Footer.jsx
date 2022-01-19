c596dffaa78b44b011e21b9fd66c87522ab5cb5b
AlexRuus <alexrus2503@gmail.com>
2022-01-19 19:32:05 +0200

    latest

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import './Footer.css'
 import TextField from "@material-ui/core/TextField";
 import Button from '@material-ui/core/Button'
 import SendIcon from '@mui/icons-material/Send';
-import { makeStyles } from '@material-ui/core'
+import { makeStyles, ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { pink } from '@mui/material/colors'
 import { useState } from 'react';
 
 
@@ -11,8 +12,8 @@ const useStyles = makeStyles({
     field: {
       marginTop: 20,
       marginBottom: 20,
+      fontSize: 24,
       display: 'flex',
-      border: 0,
     }
   })
 
@@ -26,6 +27,35 @@ const useStylesBtn = makeStyles({
           color: '#fc8474',
           backgroundColor: '#c1c1c1'
       }
+    }
+  })
+
+  const theme = createMuiTheme({
+    palette : {
+        primary: {
+            main: pink[100]
+        },
+        text: {
+            secondary: pink[100]
+        }
+    },
+    overrides: {
+        MuiInputBase: {
+            root : {
+                    fontSize: 24
+            }
+        },
+        MuiOutlinedInput : {
+            root: {
+                '& $notchedOutline': {
+                    borderColor: "white"
+                  },
+                  "&:hover $notchedOutline": {
+                    borderColor: "none"
+                  },
+            }
+        },
+
     }
   })
 
@@ -66,62 +96,61 @@ function Footer() {
     }
 
     return (
-        <div className='footer-content' >
-            <div className="my-info">
-                <p>Name</p>
-                <p>Phone</p>
-                <p>Email</p>
-                <p>Adress</p>
+    <ThemeProvider theme={theme}>
+            <div className='footer-content' >
+                <div className="my-info">
+                    <p>Name</p>
+                    <p>Phone</p>
+                    <p>Email</p>
+                    <p>Adress</p>
+                </div>
+                <div className="contact-form">
+                    <form className= 'form-fields' noValidate autoComplete='off' onSubmit={handleSubmit}>
+                        <TextField
+                        onChange={(e) => setName(e.target.value)}
+                        className={classes.field}
+                        label='Name' 
+                        variant='outlined'
+                        required
+                        error={nameError}
+                        />
+                        <TextField
+                        onChange={(e) => setSurname(e.target.value)}
+                        className={classes.field}
+                        label='Surname' 
+                        variant='outlined'
+                        required
+                        error={surnameError}
+                        />
+                        <TextField
+                        onChange={(e) => setEmail(e.target.value)}
+                        className={classes.field}
+                        label='E-mail'
+                        variant='outlined'
+                        required
+                        error={emailError}
+                        />
+                        <TextField
+                        className={classes.field}
+                        label='Send me a message...'
+                        multiline
+                        rows={6} 
+                        fullWidth
+                        variant='outlined'
+                        />
+                        
+                        <Button
+                        className={btnClass.btn}
+                        type="submit" 
+                        color="primary" 
+                        variant="contained"
+                        endIcon={<SendIcon />}>
+                        Submit
+                        </Button>
+                    </form>
+                </div>
             </div>
-            <div className="contact-form">
-                <form className= 'form-fields' noValidate autoComplete='off' onSubmit={handleSubmit}>
-                    <TextField
-                    onChange={(e) => setName(e.target.value)}
-                    className={classes.field}
-                    // color=''
-                    label='Name' 
-                    variant='outlined'
-                    required
-                    error={nameError}
-                    />
-                    <TextField
-                    onChange={(e) => setSurname(e.target.value)}
-                    className={classes.field}
-                    label='Surname' 
-                    variant='outlined'
-                    required
-                    error={surnameError}
-                    />
-                    <TextField
-                    onChange={(e) => setEmail(e.target.value)}
-                    className={classes.field}
-                    // color=''
-                    label='E-mail'
-                    variant='outlined'
-                    required
-                    error={emailError}
-                    />
-                    <TextField
-                    className={classes.field}
-                    // color='red'
-                    label='Send me a message...'
-                    multiline
-                    rows={6} 
-                    fullWidth
-                    variant='outlined'
-                    />
-                    
-                    <Button
-                    className={btnClass.btn}
-                    type="submit" 
-                    color="primary" 
-                    variant="contained"
-                    endIcon={<SendIcon />}>
-                    Submit
-                    </Button>
-                </form>
-            </div>
-        </div>
+       </ThemeProvider>
     )
 }
 
